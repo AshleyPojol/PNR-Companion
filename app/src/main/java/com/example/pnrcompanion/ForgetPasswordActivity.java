a6a6014eea92a9e9629065019e09f3fb1a117364
Maldsuro <maldsuro@gmail.com>
2022-11-01 13:57:11 +0800

    Change Activity

--- app/src/main/java/com/example/pnrcompanion/ForgetPasswordActivity.java
+++ app/src/main/java/com/example/pnrcompanion/ForgetPasswordActivity.java
@@ -1,12 +1,19 @@
 package com.example.pnrcompanion;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.w3c.dom.Text;
+
 public class ForgetPasswordActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +23,6 @@ public class ForgetPasswordActivity extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_forget_password);
-
 
     }
 }
--- app/src/main/java/com/example/pnrcompanion/LoginActivity.java
+++ app/src/main/java/com/example/pnrcompanion/LoginActivity.java
@@ -1,12 +1,22 @@
 package com.example.pnrcompanion;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.ClickableSpan;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity<clickableSpan> extends AppCompatActivity {
+    private Button btn_si;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +26,19 @@ public class LoginActivity extends AppCompatActivity {
         getSupportActionBar().hide();
         setContentView(R.layout.activity_login);
 
-
-
-
-
-
+        btn_si = (Button)findViewById(R.id.btn_si);
+        btn_si.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivityProfile();
+            }
+        });
 
     }
+
+    private void openActivityProfile() {
+        Intent intent = new Intent(this, ProfileActivity.class);
+        startActivity(intent);
+    }
+
 }
